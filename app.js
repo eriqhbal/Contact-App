300ae3ba12928ad15f22c81a022e0648d4355673
Zuleriqhbal <eriqhbal@gmail.com>
2022-11-06 14:39:35 +0700

    Membuat kondisi apakah folder atau file json ada

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
 const fs = require('fs');
 const readline = require('readline');
 
+const rl = readline.createInterface({
+   input: process.stdin,
+   output: process.stdout,
+});
+
+// membuat folder jika directory tidak ada
+const dirPath = './data';
+if(!fs.existsSync(dirPath)) {
+   fs.mkdirAsync(dirPath);
+}
+
+// apakah ada file Contacts.json
+const filePath = './data/contacts.json';
+if(!fs.existsSync(filePath)) {
+   fs.writeFileAsync(filePath,[], 'utf-8');
+}

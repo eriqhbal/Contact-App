5c3c13fb50bc9a514202d8f463102326a1cd9a59
Zuleriqhbal <eriqhbal@gmail.com>
2022-11-06 15:19:04 +0700

    Menyelesaikan program sederhana untuk membuat program contact app

--- app.js
+++ app.js
@@ -9,11 +9,46 @@ const rl = readline.createInterface({
 // membuat folder jika directory tidak ada
 const dirPath = './data';
 if(!fs.existsSync(dirPath)) {
-   fs.mkdirAsync(dirPath);
+   fs.mkdirSync(dirPath);
 }
 
 // apakah ada file Contacts.json
 const filePath = './data/contacts.json';
 if(!fs.existsSync(filePath)) {
-   fs.writeFileAsync(filePath,[], 'utf-8');
+   fs.writeFileSync(filePath, '[]', 'utf-8');
 }
+
+// Membuat Pertanyaan 
+const masukkanNama = () => {
+   return new Promise((resolve,reject) => {
+      rl.question('Masukkan Nama Anda : ', (nama) => {
+         resolve(nama);
+      })
+   })
+}
+
+const masukkanNomerHp = () => {
+  return new Promise((resolve, reject) => {
+    rl.question("Masukkan Nomer HP : ", (noHP) => {
+      resolve(noHP);
+    });
+  });
+};
+
+// Membuat fungsi utama dari Pertanyaan diatas
+const main = async () => {
+   const namaAnda = await masukkanNama();
+   const noHpAnda = await masukkanNomerHp();
+
+   const contact = { namaAnda, noHpAnda };
+   const fileBuffer = fs.readFileSync('data/contacts.json', 'utf-8');
+   const contacts = JSON.parse(fileBuffer);
+
+   contacts.push(contact);
+
+   fs.writeFileSync('data/contacts.json', JSON.stringify(contacts));
+
+   console.log('Terimakasih sudah mengisi data anda');
+}
+
+main();
